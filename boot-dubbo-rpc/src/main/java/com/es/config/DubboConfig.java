b24a32e18d21a6a3f449e86584930cc6648aa7f9
tianwamg <987897856@qq.com>
2021-08-03 16:38:44 +0800

    dubbo jsonrpc

--- boot-dubbo-rpc/src/main/java/com/es/config/DubboConfig.java
+++ boot-dubbo-rpc/src/main/java/com/es/config/DubboConfig.java
@@ -1,14 +1,18 @@
 package com.es.config;
 
 import org.apache.dubbo.config.ProtocolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DubboConfig {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @Bean
     public ProtocolConfig jsonRpc(){
-        return new ProtocolConfig("jsonrpc",10082);
+        return new ProtocolConfig("jsonrpc",port);
     }
 }
